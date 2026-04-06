ff4e583ba3118b8e11609eee58805ff5d078ac77
wlqgkrry <wlqgkrry@gmail.com>
2026-04-06 15:37:32 +0900

    first commit

---
+++ src/main/java/com/neroplan/neroplan/NeroplanApplication.java
@@ -0,0 +1,13 @@
+package com.neroplan.neroplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeroplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeroplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neroplan/neroplan/NeroplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neroplan.neroplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeroplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
