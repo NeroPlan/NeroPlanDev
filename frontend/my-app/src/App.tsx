58a44c1327b71b63204518a307778527f2856f98
yeoshin <yeoshin@yeoshinui-MacBookPro.local>
2026-05-20 15:37:08 +0900

    FEAT: add login color

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -2,39 +2,51 @@
 import './App.css'
 
 function Header() {
-    return <h1 className="text-2xl font-bold mb-10"></h1>
+    return (
+        <header className="w-full h-8 bg-white flex items-center justify-center px-6">
+            <h1 className="text-2xl font-bold">NeroPlan</h1>
+        </header>
+    )
 }
 
 function App() {
-    const handleLogin = () => {
+    const handleGoogleLogin = () => {
         window.location.href = "http://localhost:4000/api/v1/auth/login/google"
+    }
+    const handleCacaoLogin = () => {
+        window.location.href = "http://localhost:4000/api/v1/auth/login/cacao"
+    }
+    const handleNaverLogin = () => {
+        window.location.href = "http://localhost:4000/api/v1/auth/login/naver"
     }
 
     return (
-        <div className="flex flex-col items-center justify-start h-screen pt-40">
+        <div className="flex flex-col items-center justify-start h-screen pt-80">
             <Header />
-            <div className="flex flex-col gap-64 w-64">
+            <main className="flex-1 flex items-start justify-center pt-20">
+            <div className="flex flex-col gap-12 w-64">
                 <button
-                    onClick={handleLogin}
-                    className="px-10 py-6 bg-white border border-black text-black rounded"
+                    onClick={handleGoogleLogin}
+                    className="px-10 py-4 bg-white border border-black text-black rounded"
                 >
-                    구글 로그인
+                    구글 계정으로 로그인
                 </button>
 
                 <button
-                    onClick={handleLogin}
-                    className="px-10 py-6 bg-white border border-black text-black rounded"
+                    onClick={handleCacaoLogin}
+                    className="px-10 py-4 bg-[#FEE500] border border-black text-black rounded"
                 >
-                    카카오 로그인
+                    카카오 계정으로 로그인
                 </button>
 
                 <button
-                    onClick={handleLogin}
-                    className="px-10 py-6 bg-white border border-black text-black rounded"
+                    onClick={handleNaverLogin}
+                    className="px-10 py-4 bg-[#03C75A] border border-black text-black rounded"
                 >
-                    네이버 로그인
+                    네이버 계정으로 로그인
                 </button>
             </div>
+            </main>
         </div>
     )
 }
