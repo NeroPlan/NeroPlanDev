bfaf352a8e2d1fae4d0526c5e85ca206353affb1
yeoshin <yeoshin@yeoshinui-MacBookPro.local>
2026-05-28 09:17:31 +0900

    [FEAT] add login domain icon

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,5 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
+import { FcGoogle } from "react-icons/fc";
+import { SiNaver, SiKakaotalk } from "react-icons/si";
 
 function Header() {
     return (
@@ -23,29 +25,34 @@ function App() {
     return (
         <div className="flex flex-col items-center justify-start h-screen pt-80">
             <Header />
-            <main className="flex-1 flex items-start justify-center pt-20">
-            <div className="flex flex-col gap-12 w-64">
-                <button
-                    onClick={handleGoogleLogin}
-                    className="px-10 py-4 bg-white border border-black text-black rounded"
-                >
-                    구글 계정으로 로그인
-                </button>
+            <main className="flex-1 flex items-start justify-center pt-5">
+                <div className="flex min-h-screen items-center justify-center">
+                    <div className="flex flex-col gap-12 w-64">
+                        <button
+                            onClick={handleGoogleLogin}
+                            className="flex items-center justify-center gap-3 rounded-2xl border border-gray-300 bg-white py-3 px-5 text-xl font-semibold shadow-md transition hover:-translate-y-1 hover:shadow-xl"
+                        >
+                            <FcGoogle size={32} />
+                            <span>Google 로그인</span>
+                        </button>
 
-                <button
-                    onClick={handleCacaoLogin}
-                    className="px-10 py-4 bg-[#FEE500] border border-black text-black rounded"
-                >
-                    카카오 계정으로 로그인
-                </button>
+                        <button
+                            onClick={handleCacaoLogin}
+                            className="flex items-center justify-center gap-3 rounded-2xl bg-[#FEE500] py-3 px-5 text-xl font-semibold text-black shadow-md transition hover:-translate-y-1 hover:shadow-xl"
+                        >
+                            <SiKakaotalk size={30} />
+                            <span>카카오 로그인</span>
+                        </button>
 
-                <button
-                    onClick={handleNaverLogin}
-                    className="px-10 py-4 bg-[#03C75A] border border-black text-black rounded"
-                >
-                    네이버 계정으로 로그인
-                </button>
-            </div>
+                        <button
+                            onClick={handleNaverLogin}
+                           className="flex items-center justify-center gap-3 rounded-2xl bg-[#03C75A] py-3 px-5 text-xl font-semibold text-white shadow-md transition hover:-translate-y-1 hover:shadow-xl"
+                        >
+                            <SiNaver size={28} />
+                            <span>네이버 로그인</span>
+                        </button>
+                    </div> 
+                </div>
             </main>
         </div>
     )
