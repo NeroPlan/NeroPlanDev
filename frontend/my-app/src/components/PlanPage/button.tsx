a8f13b9c54b998ec8d2121dacf13171e0cd2505c
yeoshin <yeoshin@yeoshinui-MacBookPro.local>
2026-06-03 23:18:56 +0900

    FEAT: add analysis button

---
+++ frontend/my-app/src/components/PlanPage/button.tsx
@@ -0,0 +1,21 @@
+// components/PlanPage/AnalyzeButton.tsx
+
+export default function AnalyzeButton() {
+    return (
+        <button
+            className="
+                w-full
+                bg-blue-600
+                text-white
+                font-bold
+                py-3
+                rounded-2xl
+                shadow-sm
+                hover:bg-blue-700
+                transition
+            "
+        >
+            분석하기
+        </button>
+    );
+}
